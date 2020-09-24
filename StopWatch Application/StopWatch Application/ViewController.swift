36fb2d5646d4d1f482676dcc3807eb87bddf0927
Talgar <talgarrmelisbekov@gmail.com>
2020-09-24 23:47:28 +0600

    added constraints

--- StopWatch Application/StopWatch Application/ViewController.swift
+++ StopWatch Application/StopWatch Application/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
    
+    var player = AVAudioPlayer()
 
-
+    var toggleState = 1
     
-
+    @IBAction func playPause(_ sender: UIButton) {
     
+        var playBtn = sender as! UIButton
+        if toggleState == 1 {
+            player.play()
+            toggleState = 2
+            playBtn.setImage(UIImage(named:"pause"),for:UIControl.State.normal)
+        } else {
+            player.pause()
+            toggleState = 1
+            playBtn.setImage(UIImage(named:"play"),for:UIControl.State.normal)
+        }
+    }
+
+    @IBAction func updateButton(_ sender: Any) {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
